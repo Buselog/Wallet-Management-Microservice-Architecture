4d9c1c46726b9d2305169be726c0e1f7058d7499
Buselog <gorcumbuse@gmail.com>
2026-03-29 23:53:23 +0300

    Updating the exchange rate logic for null data received over the weekend

--- WalletManagement.Microservices/Investment/Infrastructure/Investment.InnerInfrastructure/Services/InvestmentRateService.cs
+++ WalletManagement.Microservices/Investment/Infrastructure/Investment.InnerInfrastructure/Services/InvestmentRateService.cs
@@ -19,11 +19,12 @@ namespace Investment.InnerInfrastructure.Services
 
         public async Task<List<ExchangeRateDto>> GetDailyRatesAsync()
         {
-            var today = DateTime.Now.ToString("dd-MM-yyyy");
+            var startDate = DateTime.Now.AddDays(-4).ToString("dd-MM-yyyy");
+            var endDate = DateTime.Now.ToString("dd-MM-yyyy");
 
             var series = "TP.DK.USD.A.YTL-TP.DK.USD.S.YTL-TP.DK.EUR.A.YTL-TP.DK.EUR.S.YTL-TP.DK.GBP.A.YTL-TP.DK.GBP.S.YTL-TP.DK.CHF.A.YTL-TP.DK.CHF.S.YTL-TP.DK.JPY.A.YTL-TP.DK.JPY.S.YTL";
 
-            var url = $"series={series}&startDate={today}&endDate={today}&type=json";
+            var url = $"series={series}&startDate={startDate}&endDate={endDate}&type=json";
 
             _httpClient.DefaultRequestHeaders.Clear();
             _httpClient.DefaultRequestHeaders.Add("key", _apiKey);
@@ -36,20 +37,23 @@ namespace Investment.InnerInfrastructure.Services
             var jsonContent = await response.Content.ReadAsStringAsync();
 
             using var doc = JsonDocument.Parse(jsonContent);
-            var items = doc.RootElement.GetProperty("items");
+            var items = doc.RootElement.GetProperty("items").EnumerateArray().ToList();
 
-            if (items.GetArrayLength() == 0) return new List<ExchangeRateDto>();
+            var latestValidItem = items
+                .Where(x => x.TryGetProperty("TP_DK_USD_A_YTL", out var val) && val.ValueKind != JsonValueKind.Null)
+                .LastOrDefault();
 
-            var firstItem = items[0];
+            if (latestValidItem.ValueKind == JsonValueKind.Undefined) return new List<ExchangeRateDto>();
 
+         
             return new List<ExchangeRateDto>
             {
 
-                CreateDto("USD", firstItem, "TP_DK_USD_A_YTL", "TP_DK_USD_S_YTL"),
-                CreateDto("EUR", firstItem, "TP_DK_EUR_A_YTL", "TP_DK_EUR_S_YTL"),
-                CreateDto("GBP", firstItem, "TP_DK_GBP_A_YTL", "TP_DK_GBP_S_YTL"),
-                CreateDto("CHF", firstItem, "TP_DK_CHF_A_YTL", "TP_DK_CHF_S_YTL"),
-                CreateDto("JPY", firstItem, "TP_DK_JPY_A_YTL", "TP_DK_JPY_S_YTL")
+                CreateDto("USD", latestValidItem, "TP_DK_USD_A_YTL", "TP_DK_USD_S_YTL"),
+                CreateDto("EUR", latestValidItem, "TP_DK_EUR_A_YTL", "TP_DK_EUR_S_YTL"),
+                CreateDto("GBP", latestValidItem, "TP_DK_GBP_A_YTL", "TP_DK_GBP_S_YTL"),
+                CreateDto("CHF", latestValidItem, "TP_DK_CHF_A_YTL", "TP_DK_CHF_S_YTL"),
+                CreateDto("JPY", latestValidItem, "TP_DK_JPY_A_YTL", "TP_DK_JPY_S_YTL")
             };
 
         }
