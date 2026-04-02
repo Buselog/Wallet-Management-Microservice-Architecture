b00a327bf5ff48dfe0874c0a8d0e9e66040dc3b5
Buselog <gorcumbuse@gmail.com>
2026-04-02 10:03:19 +0300

    Added LanguageClient plugin for GetTranslation

---
+++ WalletManagement.Microservices/Shared/Core/MultiLanguage.Client/Abstracts/ILanguageClient.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MultiLanguage.Client.Abstracts
+{
+    public interface ILanguageClient
+    {
+         public Task<string> GetTranslationAsync(string key, string cultureCode);
+    }
+}
+
---
+++ WalletManagement.Microservices/Shared/Core/MultiLanguage.Client/Concretes/LanguageClient.cs
@@ -0,0 +1,40 @@
+﻿using MultiLanguage.Client.Abstracts;
+using System.Net.Http.Headers;
+
+namespace MultiLanguage.Client.Concretes
+{
+    public class LanguageClient : ILanguageClient
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public LanguageClient(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<string> GetTranslationAsync(string key, string cultureCode)
+        {
+            try
+            {
+                var client = _clientFactory.CreateClient("MultiLanguageAPI");
+
+                client.DefaultRequestHeaders.AcceptLanguage.Clear();
+                client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureCode));
+
+                var response = await client.GetAsync($"api/resource/get-translation?key={key}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+                return key;
+
+            }
+            catch (Exception)
+            {
+                return key;
+            }
+        }
+    }
+}
