b4b1cb3a42e941b7914f3e756075bec642914d84
Buselog <gorcumbuse@gmail.com>
2026-04-27 12:22:43 +0300

    Example controller writing

---
+++ WalletManagement.Microservices/Gateway/WM.Gateway/Controllers/WalletProxyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WM.Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletProxyController : ControllerBase
+    {
+    }
+}
